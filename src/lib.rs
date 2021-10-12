87d2f6805bfcb695cbba8be6789d325ce0ec8866
Marcos Gutiérrez Alonso <margual56@gmail.com>
2021-10-12 20:55:24 +0200

    Added colored output and multiple patterns

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,45 @@ use std::io::{
 	Write as IoWrite,
 };
 use anyhow::{Context, Result};
+use termcolor::{ColorSpec, WriteColor};
 
-pub fn find_matches(content: Box<dyn BufRead>, pattern: &str, mut writer: impl IoWrite) -> Result<()>  {
+
+pub fn find_matches(content: Box<dyn BufRead>, patterns: Vec<String>, mut writer: impl IoWrite) -> Result<()>  {
     for line in content.lines() {
 		let l = line.unwrap();
-		if l.to_lowercase().contains(pattern) {
-			writeln!(writer, "{}", l).with_context(|| format!("Could not write to the buffer. Maybe it is full"))?;
+		for pattern in &patterns {
+			if l.contains(pattern) {
+				writeln!(writer, "{}", l).with_context(|| format!("Could not write to the buffer. Maybe it is full"))?;
+			}
 		}
     }
+
+	Ok(())
+}
+
+pub fn find_matches_color_word(content: Box<dyn BufRead>, patterns: Vec<String>, mut writer: impl WriteColor, color: &ColorSpec) -> Result<()>  {
+    for line in content.lines() {
+		let l = line.unwrap();
+		for pattern in &patterns {
+			if l.contains(pattern) {
+				let parts: Vec<&str> = l.split(pattern).collect();
+				let last = parts[parts.len()-1];
+
+				for part in parts {
+					if part == last {continue}
+					
+					writer.reset()?; // Reset the color
+					write!(writer, "{}", part).with_context(|| format!("Could not write to the buffer. Maybe it is full"))?;
+
+					writer.set_color(color)?;
+					write!(writer, "{}", pattern).with_context(|| format!("Could not write to the buffer. Maybe it is full"))?;
+				}
+
+				writer.reset()?; // Reset the color
+				writeln!(writer, "{}", last)?;
+			}
+		}
+	}
 
 	Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,45 @@ use std::io::{
 	BufRead
 };
 use anyhow::{Result};
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream};
 
 
 #[derive(StructOpt)]
 #[structopt(about = "Just a simple grep clone programmed in Rust. \ngrepclone takes either an input file (`-i` option) or reads the stdin if no file is passed as an argument.")]
 struct Cli {
-	pattern: String,
+	/// Pattern(s) to search for. Required parameter.
+	patterns: Vec<String>,
+
+	/// Input file (Optional)
 	#[structopt(short = "i", long = "input", parse(from_os_str))]
     input: Option<std::path::PathBuf>,
+
+	/// Wether to print with colored output or not
+	#[structopt(short = "c", long = "color", parse(from_flag))]
+	color: bool,
 }
 
 fn main() -> Result<()> {
     let args = Cli::from_args();
 	
+	// Detect wether a file has been passed as an argument
+	// or if it has to be read from stdin
 	let reader: Box<dyn BufRead> = match args.input {
         None => Box::new(BufReader::new(std::io::stdin())),
         Some(path) => Box::new(BufReader::new(File::open(path)?))
     };
 
-	grepclone::find_matches(reader, &args.pattern, &mut std::io::stdout())?;
+	// If the color flag has been passed, then create a special stdout with colors
+	if args.color {
+		let stdout = StandardStream::stdout(ColorChoice::Always);
+
+		let mut color = ColorSpec::new();
+		color.set_fg(Some(Color::Red));
+
+		grepclone::find_matches_color_word(reader, args.patterns, stdout, &color)?;
+	}else{
+		grepclone::find_matches(reader, args.patterns, std::io::stdout())?;
+	}
 
 	Ok(())
 } 
